97c346d795dccb94c7112d2bb198f6384137f229
Villa <digitalvilla1@gmail.com>
2019-02-07 16:03:02 -0700

    Deployed to heroku

---
+++ client/build/precache-manifest.a14bce3e3d3145a424d0d63381eac7b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "203937b961e450e7cca73f1c4a5b3bb6",
+    "url": "/static/media/sprite.203937b9.svg"
+  },
+  {
+    "revision": "83b8edb9f7ef850f7eeaac636c9f03cc",
+    "url": "/static/media/colibri.83b8edb9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "55a233d86f5069e507fa",
+    "url": "/static/js/main.55a233d8.chunk.js"
+  },
+  {
+    "revision": "2eab6a9563a35066de3b",
+    "url": "/static/js/1.2eab6a95.chunk.js"
+  },
+  {
+    "revision": "55a233d86f5069e507fa",
+    "url": "/static/css/main.3bce13c5.chunk.css"
+  },
+  {
+    "revision": "8afbc95753bdd384e59060363c88ff66",
+    "url": "/index.html"
+  }
+];
