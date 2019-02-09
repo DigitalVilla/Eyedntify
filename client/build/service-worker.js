5b6452a05c7111519ecacf39cfc735c26a9a1cab
Villa <digitalvilla1@gmail.com>
2019-02-08 18:44:29 -0700

    Heroku Deployment

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e177f0e28ba48db092f51960ca467d8b.js"
+  "/precache-manifest.747a99f2b60663bf482d0d8567eeebdb.js"
 );
 
 workbox.clientsClaim();
